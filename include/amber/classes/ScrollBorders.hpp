8379f6e26c31bc0c41b2e0b14adec8c6d20a1f84
Fryy_55 <ivan_ugu55@icloud.com>
2026-04-05 22:59:16 +0500

    Completely revamp `ScrollBorders`

--- include/amber/classes/ScrollBorders.hpp
+++ include/amber/classes/ScrollBorders.hpp
@@ -2,6 +2,8 @@
 
 #include <Geode/cocos/base_nodes/CCNode.h>
 
+#include <amber/classes/ColoredLabel.hpp>
+
 
 namespace amber {
 
@@ -13,23 +15,35 @@ public:
 		char const* title,
 		char const* font = "bigFont.fnt"
 	);
+	static ScrollBorders* create(geode::ScrollLayer* childScrollLayer);
 
 private:
 	bool init(cocos2d::CCSize const&, char const*, char const*);
 
-	// Fields
-	cocos2d::CCLabelBMFont* m_title;
-	cocos2d::extension::CCScale9Sprite* m_top;
-	cocos2d::extension::CCScale9Sprite* m_bottom;
-	cocos2d::extension::CCScale9Sprite* m_left;
-	cocos2d::extension::CCScale9Sprite* m_right;
+public:
+	void addBackground(geode::ZStringView sprite = "GJ_square01.png");
 
-	static constexpr float s_sideSpriteOffset = 15.f;
+private:
+	void positionBG();
 
 public:
+	void setTitle(geode::ZStringView title);
+	void setString(geode::ZStringView title);
+
 	void setContentSize(cocos2d::CCSize const& size) override;
 	void setContentWidth(float width) { setContentSize({ width, getContentHeight() }); }
 	void setContentHeight(float height) { setContentSize({ getContentWidth(), height }); }
+
+// Fields
+private:
+	static constexpr float s_sideSpriteOffset = 15.f;
+
+	ColoredLabel* m_title;
+	geode::NineSlice* m_top;
+	geode::NineSlice* m_bottom;
+	cocos2d::extension::CCScale9Sprite* m_left;
+	cocos2d::extension::CCScale9Sprite* m_right;
+	geode::NineSlice* m_bg = nullptr;
 };
 
 } // namespace amber
--- src/classes/ScrollBorders.cpp
+++ src/classes/ScrollBorders.cpp
@@ -20,19 +20,30 @@ ScrollBorders* ScrollBorders::create(CCSize const& size, char const* title, char
 	return nullptr;
 }
 
+ScrollBorders* ScrollBorders::create(ScrollLayer* childScrollLayer) {
+	auto ret = create(
+		childScrollLayer->getScaledContentSize() + CCSize(40.f, 23.f)
+	);
+
+	childScrollLayer->setPosition(20.f, 31.f);
+	ret->addChild(childScrollLayer);
+
+	return ret;
+}
+
 bool ScrollBorders::init(CCSize const& size, char const* title, char const* font) {
 	if (!CCNode::init())
 		return false;
 
-	m_title = CCLabelBMFont::create(title, font);
+	m_title = ColoredLabel::create(title, font);
 	m_title->setID("title");
 	this->addChild(m_title, 11);
 
-	m_top = CCScale9Sprite::createWithSpriteFrameName("GJ_table_top_001.png");
+	m_top = NineSlice::createWithSpriteFrameName("GJ_table_top_001.png");
 	m_top->setID("top-sprite");
 	this->addChild(m_top, 10);
 
-	m_bottom = CCScale9Sprite::createWithSpriteFrameName("GJ_table_bottom_001.png");
+	m_bottom = NineSlice::createWithSpriteFrameName("GJ_table_bottom_001.png");
 	m_bottom->setID("bottom-sprite");
 	this->addChild(m_bottom, 10);
 
@@ -51,23 +62,60 @@ bool ScrollBorders::init(CCSize const& size, char const* title, char const* font
 	return true;
 }
 
-void ScrollBorders::setContentSize(CCSize const& size) {
-	CCNode::setContentSize(size);
+void ScrollBorders::addBackground(ZStringView sprite) {
+	if (m_bg)
+		m_bg->removeFromParent();
 
-	m_title->setPosition(size.width / 2.f, size.height + 8.f);
+	m_bg = NineSlice::create(sprite);
+	m_bg->setID("background");
+	this->addChild(m_bg, -10);
+
+	this->positionBG();
+
+	return;
+}
+
+void ScrollBorders::positionBG() {
+	auto size = this->getContentSize();
+
+	m_bg->setPosition(size / 2.f);
+	m_bg->setContentSize(size - CCSize(25.f, 45.f));
+
+	return;
+}
+
+void ScrollBorders::setTitle(ZStringView title) {
+	m_title->setText(title);
+
+	return;
+}
+
+void ScrollBorders::setString(ZStringView title) {
+	m_title->setString(title.c_str());
+
+	return;
+}
+
+void ScrollBorders::setContentSize(CCSize const& size) {
+	CCNode::setContentSize(size + CCSize(0.f, 40.f));
+
+	m_title->setPosition(size.width / 2.f, size.height + 27.f);
 	m_title->limitLabelWidth(size.width - 70.f, 0.95f, 0.1f);
 
-	m_top->setPosition(size.width / 2.f, size.height);
+	m_top->setPosition(size.width / 2.f, size.height + 19.f);
 	m_top->setContentWidth(size.width);
 
-	m_bottom->setPosition(size.width / 2.f, 0.f);
+	m_bottom->setPosition(size.width / 2.f, 19.f);
 	m_bottom->setContentWidth(size.width - 2.f); // adjust for side sprites
 
-	m_left->setPosition(s_sideSpriteOffset,	size.height / 2.f);
+	m_left->setPosition(s_sideSpriteOffset,	size.height / 2.f + 19.f);
 	m_left->setContentHeight(size.height);
 
-	m_right->setPosition(size.width - s_sideSpriteOffset, size.height / 2.f);
+	m_right->setPosition(size.width - s_sideSpriteOffset, size.height / 2.f + 19.f);
 	m_right->setContentHeight(size.height);
+
+	if (m_bg)
+		this->positionBG();
 
 	return;
 }
