6e27048759adec8b766cd1ba9e1cf28bd5d4c68a
Fryy_55 <3pm.fryy@gmail.com>
2026-04-24 01:13:06 +0500

    Transition `ContentTo`
    - `AMBER_DLL`
    - No `final`; `private` -> `protected`

--- include/actions/ContentTo.hpp
+++ include/actions/ContentTo.hpp
@@ -1,21 +1,24 @@
 #pragma once
 
+#include "../_internal/common.hpp"
+
 #include <Geode/cocos/actions/CCActionInterval.h>
 
 
 namespace amber::actions {
 
-class ContentTo final : public cocos2d::CCActionInterval {
+class AMBER_DLL ContentTo : public cocos2d::CCActionInterval {
 public:
 	static ContentTo* create(float duration, float width = 0.f, float height = 0.f);
 	static ContentTo* create(float duration, cocos2d::CCSize const& size);
 
-private:
+protected:
 	bool initWithDuration(float, float, float);
 	void startWithTarget(cocos2d::CCNode*) override;
 	void update(float) override;
 
-	// Fields
+// Fields
+protected:
 	float m_startW;
 	float m_startH;
 	float m_deltaW;
