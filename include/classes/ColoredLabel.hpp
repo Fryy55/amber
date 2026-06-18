567be06bbac2c56a962533765b275bcbd4debc34
Fryy_55 <3pm.fryy@gmail.com>
2026-06-18 18:31:53 +0500

    Transition `ColoredLabel`
    - `AMBER_DLL`
    - No `final`; `private` -> `protected`

--- include/classes/ColoredLabel.hpp
+++ include/classes/ColoredLabel.hpp
@@ -1,28 +1,29 @@
 #pragma once
 
+#include "../_internal/common.hpp"
 
 #include <Geode/cocos/label_nodes/CCLabelBMFont.h>
 
 
 namespace amber {
 
-class ColoredLabel final : public cocos2d::CCLabelBMFont {
+class AMBER_DLL ColoredLabel : public cocos2d::CCLabelBMFont {
 public:
 	static ColoredLabel* create(geode::ZStringView text, geode::ZStringView font = "bigFont.fnt", bool nullptrOnFail = false);
 
-private:
+protected:
 	bool init(geode::ZStringView, geode::ZStringView, bool);
 
 public:
 	void setText(geode::ZStringView text);
 
-private:
+protected:
 	bool renderText(geode::ZStringView);
 	std::optional<std::string> collectTag(std::size_t curPos, std::string_view string);
 	cocos2d::ccColor3B colorForTag(std::string_view tag);
 
 // Fields
-private:
+protected:
 	static constexpr char const* s_errorText = "<cr>Failed to parse text.</c>";
 };
 
