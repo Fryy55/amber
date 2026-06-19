bfbd73ec866ef7bf9782d1e13bbf2613a530069a
Fryy_55 <3pm.fryy@gmail.com>
2026-06-19 22:09:05 +0500

    Transition `geometry.hpp`
    - `AMBER_DLL`

--- include/utils/geometry.hpp
+++ include/utils/geometry.hpp
@@ -1,12 +1,18 @@
 #pragma once
 
+#include "../_internal/common.hpp"
+
 #include <Geode/cocos/cocoa/CCGeometry.h>
 
 
 namespace amber::utils {
 
-cocos2d::CCRect convertBoxToWorldSpace(cocos2d::CCNode* node) noexcept;
+[[nodiscard]] AMBER_DLL cocos2d::CCRect convertBoxToWorldSpace(
+	cocos2d::CCNode* node
+) noexcept;
 
-bool rectContains(cocos2d::CCRect const& outer, cocos2d::CCRect const& inner) noexcept;
+[[nodiscard]] AMBER_DLL bool rectContains(
+	cocos2d::CCRect const& outer, cocos2d::CCRect const& inner
+) noexcept;
 
 } // namespace amber::utils
